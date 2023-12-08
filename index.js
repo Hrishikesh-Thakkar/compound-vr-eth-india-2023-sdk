15368fec4cf637ad2abec56f26a8aebf0c87679a
Rahul Gupta <rahul7668gupta@gmail.com>
2023-12-08 21:42:27 +0530

    airstack node

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const { graphql, buildSchema } = require('graphql');
 
-// Builds a schema using the GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+import { init, fetchQueryWithPagination } from "@airstack/node";
 
-// The root of our graph gives us access to resolvers for each type and field
-const resolversRoot = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
-
-// Run a simple graphql query '{ hello }' and then print the response
-graphql(schema, '{ hello }', resolversRoot).then((response) => {
-  console.log(JSON.stringify(response.data));
-});
+init(process.env.AIRSTACK_API_KEY);
